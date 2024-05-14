cefec2834c093b289dcc32a17694e3ba55b45361
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2024-05-14 13:34:50 -0300

    Add files via upload

---
+++ aulaTeste/hotel/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage),         # Inclui as urls do app blog
+    path('continueHomepage/', views.continueHomepage),
+]
