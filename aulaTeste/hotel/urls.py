3bb6d0f763c8d87bf7f9732acd140c3ca8dc93c8
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2024-05-14 21:51:21 -0300

    Delete aulaTeste directory

--- aulaTeste/hotel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage),         # Inclui as urls do app blog
-    path('continueHomepage/', views.continueHomepage),
-    path('quartos/', views.quartos),
-]
